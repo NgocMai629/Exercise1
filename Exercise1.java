54fd1033c6c41c583675f204852d28605fcbcaa7
NgocMai629 <37764151+NgocMai629@users.noreply.github.com>
2018-03-29 23:26:04 +0700

    Add files via upload

--- Exercise1.java
+++ Exercise1.java
@@ -4,38 +4,55 @@
  * and open the template in the editor.
  */
 package javaapplication1;
-import java.io.IOException;
 import java.util.*;
 
-public class JavaApplication1 {
+public class Exercise1{
 
     public static void main(String[] args) {
         int n;
         Scanner inp = new Scanner(System.in);
-        System.out.print("Input n: ");
-        n = inp.nextInt();
-        System.out.print("");
-        for (int i = 2; i <= n; i++) {
-           if (primesNumber(i)) System.out.print(i + " ");
+        
+        
+        int i = 2, dem = 0;
+        boolean flag = false;
+        while (!flag) {
+            try {
+                System.out.print("Input n: ");
+                n = Integer.parseInt(inp.nextLine());
+                if (n > 0) {
+                    while (dem < n) {
+                        if (primesNumber(i)) {
+                            System.out.print(i + " ");
+                            dem++;
+                        }
+                        i = i + 1;
+                    }                   
+                    flag = true;
+                } else {
+                    flag = false;
+                    System.out.println("Please input a positive number!");               
+                }
+            } catch (NumberFormatException e) {
+                flag = false;
+                System.out.println("Please input a positive number!");
+            }
         }
     }
-    public static boolean primesNumber(int n){
+    public static boolean primesNumber(int n) {
         int dem = 0;
         for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
                 dem++;
             }
-            }
-         if (dem == 2) {
-            return true;
         }
-         else {
+        if (dem == 2) {
+            return true;
+        } else {
             return false;
         }
-        }
-    
-    
     }
+
+}
     
 
 
