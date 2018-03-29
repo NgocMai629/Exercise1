6c6a0216193f1dcd1830eda021f62a0fc972395b
NgocMai629 <37764151+NgocMai629@users.noreply.github.com>
2018-03-29 23:26:31 +0700

    Add files via upload

--- exercise2.java
+++ exercise2.java
@@ -3,24 +3,37 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication2;
+package Ex2;
 //import java.io.IOException;
 import java.util.Scanner;
 
-public class JavaApplication2 {
+public class exercise2 {
     
     public static void main(String[] args) {
         int n; int tmp;
         Scanner inp = new Scanner(System.in);
-        System.out.print("Input n: ");
-        n = inp.nextInt();
-        System.out.print("");
-        for(int i = 1; i <= 10; i++){
-           tmp = n * i;
-           String output = String.format("%d\t*\t%d\t =\t%d\n", n, i, tmp);
-          // System.out.printf("%d * %d = %d\n", n, i, tmp);
-          System.out.printf(output);
+        boolean flag = false;
+        while (!flag) {
+            try {
+                System.out.print("Input n: ");
+                n = Integer.parseInt(inp.nextLine());
+                if (n > 1 && n < 10 ) {
+                    for(int i = 1; i <= 10; i++){
+                    tmp = n * i;
+                    String output = String.format("%8s * %4s =%4s\n", n, i, tmp);
+                    System.out.printf(output);
+                    flag = true;
+                }
+                }else 
+                {
+                    flag = false;
+                    System.out.println("Please input integer number in the range of 2 to 9!");  
+                }
+            }catch (NumberFormatException e) {
+                flag = false;
+                System.out.println("Please input integer number in the range of 2 to 9!");
+            }
+        }
        } 
     }
-    
-}
+
